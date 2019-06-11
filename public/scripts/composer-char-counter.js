ae96d1eb19f53412347bff2320d7c85b66cedf4c
Won Seob Shin <wonseob07@gmail.com>
2019-06-11 23:22:42 +0000

    added character counter for new tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  console.log('DOM is loaded and script is running!');
+
+  const tweet = document.getElementById("tweet-textarea");
+  const counter = document.getElementById("tweet-counter");
+
+  tweet.addEventListener('keyup', function(event){
+    let textLengthMax = 140;
+    let textLength = textLengthMax - this.value.length;
+    let text = this.value;
+
+    counter.innerHTML = textLength;
+
+    if(counter.innerHTML < 0){
+      counter.style.color = "red";
+    } else {
+      counter.style.color = "black";
+    }
+  });
+});
+
+
